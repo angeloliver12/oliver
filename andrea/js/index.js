ed00715d8aad4ddc7b1f1988707c4d13527badd0
angeloliver12 <87212473+angeloliver12@users.noreply.github.com>
2021-07-09 19:36:31 -0500

    Add files via upload

---
+++ andrea/js/index.js
@@ -0,0 +1,11 @@
+// Creado por oliver
+
+function proceed(){
+    document.getElementById("loaded").style="display:none"
+    var obj=document.getElementById("a");
+        obj.play();
+
+}
+alert("compilado con musica🔊");
+alert("feliz cumpleaños andreita :)");
+alert("dale me gusta si o que  ");
